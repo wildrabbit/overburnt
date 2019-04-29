ab61fa13d6aff9fefb4c0e4ea6ed04c54c5ef19d
Tania A <ithiliel@gmail.com>
2019-04-29 02:58:44 +0200

    Menu sound >_< Intro/How to screen

---
+++ Overburnt/Assets/Scripts/TransitionScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionScreen : MonoBehaviour
+{
+    public GameObject InputText;
+    public float Delay;
+    public string NextScene;
+    public AudioSource ConfirmSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputText.SetActive(false);
+        StartCoroutine(AwaitAndInput());
+    }
+
+    // Update is called once per frame
+    IEnumerator AwaitAndInput()
+    {
+        yield return new WaitForSeconds(Delay);
+        InputText.SetActive(true);
+        while(!Input.anyKey)
+        {
+            yield return null;
+        }
+        ConfirmSound.Play();
+        yield return new WaitForSeconds(ConfirmSound.clip.length);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(NextScene);
+    }
+}
